e0fe22806f3e082be67364d2f98c8f81d782bcc5
fahim ashhab <ashhabfahim@gmail.com>
2022-11-20 00:46:25 +0600

    "yoo"

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-class Car():
-    x=100
-    def __init__(self,year,make):
-        self.year=year
-        self.make=make
-        
-    @classmethod
-    def sum(cls):
-      return cls.
+# @personal_sort
 
-
-Car1=Car(200,"toyota")
-
-print(Car.sum)
