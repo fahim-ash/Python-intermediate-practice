cb432ae4a885518a06be9c105f0c6503c019e392
fahim-ash <ashhabfahim@gmail.com>
2022-11-21 16:24:15 +0600

    "4th day"

--- repr.py
+++ repr.py
@@ -1,6 +1,11 @@
-#repr returns the string representation of anything
-#calls the magic method __repr__
+# repr returns the string representation of anything
+# calls the magic method __repr__
 # repr() is used during debugging 
 z="foo"
 # print(str(z))
-print(repr(z)) 
+print(repr(z))
+mm=[12,3] 
+mmm=(str(mm))
+print(mmm)
+print(type(mmm))
+
