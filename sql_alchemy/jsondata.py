5a6c9bd5d6ea56ec23f18ce0651865502ea0575f
fahim-ash <ashhabfahim@gmail.com>
2022-11-23 23:47:03 +0600

    '5th day'

---
+++ sql_alchemy/jsondata.py
@@ -0,0 +1,26 @@
+#id,name,age,grade
+obj=[{
+
+  "name": "Jeanette",
+  "age": "10",
+  "grade": "F",
+
+}, {
+
+  "name": "Giavani",
+  "age": "21",
+  "grade": "B",
+
+}, {
+
+  "name": "Noell",
+  "age": "18",
+  "grade": "F",
+
+}, {
+
+  "name": "Willard",
+  "age": "100",
+  "grade": "A",
+
+}]
