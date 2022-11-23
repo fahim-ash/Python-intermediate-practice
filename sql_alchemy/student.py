349f01524f1d703abc0d2d0da586f737927bfc15
fahim-ash <ashhabfahim@gmail.com>
2022-11-23 23:46:50 +0600

    '5th day'

--- sql_alchemy/student.py
+++ sql_alchemy/student.py
@@ -1,14 +1,16 @@
+#--------crearting the schema--------#
 from sqlalchemy.orm import sessionmaker,declarative_base
-
 from sqlalchemy import *
 import os
+from jsondata import obj
+import sqlite3
 
 
-#sqlite3 path
+
+# sqlite3 path
 Base_dir=os.path.dirname(os.path.realpath(__file__))
-print(Base_dir)
 connection_string="sqlite:///"+os.path.join(Base_dir,'site.db')
-print(connection_string)
+
 #session
 engine = create_engine(connection_string,echo=False)
 Session=sessionmaker(bind=engine)
@@ -16,11 +18,22 @@ session=Session()
 
 Base = declarative_base()
 
-class Struden(Base):
+class Student(Base):
     __tablename__= 'student'
-    id= Column (Integer,primary_key=True)
+    id= Column (Integer,primary_key=True,autoincrement=True)
     name= Column (String(50))
     age= Column (Integer)
     grade= Column (String(50))
 
 Base.metadata.create_all(engine)
+
+#------ insert into database--------#
+# for info in obj:
+#     print(info)
+#     user=Student(name=info['name'],age=info['age'],grade=info['grade'])
+#     session.add(user)
+#     session.commit()
+
+# traverse sql
+
+check=student.select()
